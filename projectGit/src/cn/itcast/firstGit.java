1d59e14ecb43feee3ca69f9add6f2bf0ceeb54eb
andong <1428320036@qq.com>
2019-09-23 16:44:40 +0800

    本地工程添加到git

---
+++ projectGit/src/cn/itcast/firstGit.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+public class firstGit {
+    public static void main(String[] args) {
+        System.out.println("hello...");
+        System.out.println("hello...");
+        System.out.println("hello...");
+        System.out.println("hello...");
+        System.out.println("hello...");
+        System.out.println("hello...");
+        System.out.println("hello...");
+    }
+}
